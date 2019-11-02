034bae419a18de9425e9b830e72229f08b38141e
Arman Kozhakhmetov <kozhakhmetov13@gmail.com>
2019-11-02 16:03:07 +0600

    feat: import scss variables

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="">
+    <h2>Каталог фильмов</h2>
+    <input v-model="query" type="text" placeholder="Введите название">
+    <section>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-12 col-md-6 col-lg-4">
+                <div class="card">card</div>
+              </div>
+              <div class="col-12 col-md-6 col-lg-4">
+                <div class="card">card</div>
+              </div>
+              <div class="col-12 col-md-6 col-lg-4">
+                <div class="card">card</div>
+              </div>
+          </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld
+  },
+  data() {
+    return {
+      query: '',
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  $grid-gutter-width: 6px;
+  @import "bootstrap/scss/bootstrap-grid.scss";
+
+  .card {
+    background-color: yellow;
+  }
+
 </style>
